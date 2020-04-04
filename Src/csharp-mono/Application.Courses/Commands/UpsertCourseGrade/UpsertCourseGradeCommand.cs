d24f8879fc9037a7cd5160bb9d2a1b7b495c8c12
arjuns <arjuns@SDD-1>
2020-04-04 10:00:08 +0530

    minor fixes

--- Src/csharp-mono/Application.Courses/Commands/UpsertCourseGrade/UpsertCourseGradeCommand.cs
+++ Src/csharp-mono/Application.Courses/Commands/UpsertCourseGrade/UpsertCourseGradeCommand.cs
@@ -1,9 +1,7 @@
 using MediatR;
 
-namespace Application.Courses.Commands.UpsertCourseGrade
-{
-    public class UpsertCourseGradeCommand : IRequest<object>
-    {
+namespace Application.Courses.Commands.UpsertCourseGrade {
+    public class UpsertCourseGradeCommand : IRequest<object> {
         public long courseid { get; set; }
         public long gradeid { get; set; }
     }
--- Src/csharp-mono/Application.Courses/Commands/UpsertCourseGrade/UpsertCourseGradeCommandHandler.cs
+++ Src/csharp-mono/Application.Courses/Commands/UpsertCourseGrade/UpsertCourseGradeCommandHandler.cs
@@ -1,50 +1,43 @@
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using Bridge.Persistence;
-using System.Linq;
-using System;
 using Bridge.Domain.Entities;
+using Bridge.Persistence;
+using MediatR;
 
-namespace Application.Courses.Commands.UpsertCourseGrade
-{
-    public class UpsertCourseGradeCommandHandler : IRequestHandler<UpsertCourseGradeCommand, object>
-    {
+namespace Application.Courses.Commands.UpsertCourseGrade {
+    public class UpsertCourseGradeCommandHandler : IRequestHandler<UpsertCourseGradeCommand, object> {
         private readonly BridgeDbContext _dbContext;
-        public UpsertCourseGradeCommandHandler(BridgeDbContext dbContext)
-        {
+        public UpsertCourseGradeCommandHandler (BridgeDbContext dbContext) {
             _dbContext = dbContext;
         }
-  
-        public async Task<object> Handle(UpsertCourseGradeCommand request, CancellationToken cancellationToken)
-        {
+
+        public async Task<object> Handle (UpsertCourseGradeCommand request, CancellationToken cancellationToken) {
             try {
-            var isExist = _dbContext.CourseGrade.Where(x=>x.CourseId==request.courseid).FirstOrDefault();
-            if(isExist==null)
-            {
-                CourseGrade obj= new CourseGrade(){
-                CourseId = request.courseid,
-                Gradeid = request.gradeid,
-                IsDeleted = false,
-                CreatedOn = DateTime.UtcNow,
-                CreatedBy = 0
-                };
-                await _dbContext.CourseGrade.AddAsync(obj);
-                await _dbContext.SaveChangesAsync();
-            } else {
-                if(isExist.Gradeid == request.gradeid)
-                {
-                    isExist.CourseId = request.courseid;
-                    isExist.Gradeid = request.gradeid;
-                    isExist.IsDeleted = false;
-                    isExist.LastModifiedOn = DateTime.UtcNow;
-                    isExist.LastModifiedBy = 0;
-                    await _dbContext.SaveChangesAsync();
+                var isExist = _dbContext.CourseGrade.Where (x => x.CourseId == request.courseid).FirstOrDefault ();
+                if (isExist == null) {
+                    CourseGrade obj = new CourseGrade () {
+                    CourseId = request.courseid,
+                    Gradeid = request.gradeid,
+                    IsDeleted = false,
+                    CreatedOn = DateTime.UtcNow,
+                    CreatedBy = 0
+                    };
+                    await _dbContext.CourseGrade.AddAsync (obj);
+                    await _dbContext.SaveChangesAsync ();
+                } else {
+                    if (isExist.Gradeid == request.gradeid) {
+                        isExist.CourseId = request.courseid;
+                        isExist.Gradeid = request.gradeid;
+                        isExist.IsDeleted = false;
+                        isExist.LastModifiedOn = DateTime.UtcNow;
+                        isExist.LastModifiedBy = 0;
+                        await _dbContext.SaveChangesAsync ();
+                    }
                 }
-            }
-            return "ghj"; 
-            } 
-            catch(Exception ex){
+                return "ghj";
+            } catch (Exception ex) {
                 return ex;
             }
         }
