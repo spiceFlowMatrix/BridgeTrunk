a0de30a4ff09774386b7ec7c6acdb76646e95084
arjuns <arjuns@SDD-1>
2020-04-04 17:26:18 +0530

    Course Controller updated

---
+++ Src/csharp-mono/Application.Courses/Queries/GetCourseGrade/CourseGradeVm.cs
@@ -0,0 +1,9 @@
+namespace Application.Courses.Queries.GetCourseGrade
+{
+    public class CourseGradeVm
+    {
+        public long id { get; set; }
+        public long courseid { get; set; }
+        public long gradeid { get; set; }
+    }
+}
