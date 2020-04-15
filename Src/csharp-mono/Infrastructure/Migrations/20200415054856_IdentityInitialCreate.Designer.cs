3a766de9617607fa4c9c4ba9770f05e9443efa6b
ramanr <ramanrawat03@gmail.com>
2020-04-15 12:37:06 +0530

    Minor changes done for Identity

--- Src/csharp-mono/Infrastructure/Migrations/20200415054856_IdentityInitialCreate.Designer.cs
+++ Src/csharp-mono/Infrastructure/Migrations/20200415054856_IdentityInitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bridge.Infrastructure.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bridge.Infrastructure.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [DbContext(typeof(IdentityDbContext))]
+    [Migration("20200415054856_IdentityInitialCreate")]
+    partial class IdentityInitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
