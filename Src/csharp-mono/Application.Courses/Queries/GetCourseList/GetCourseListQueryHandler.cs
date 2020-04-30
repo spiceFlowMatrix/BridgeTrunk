ced3241de2b6a83968bd3c4f6fdc8c1cbb2ba632
arjuns <arjuns@SDD-1>
2020-04-30 11:01:52 +0530

    minor fixes

--- Src/csharp-mono/Application.Courses/Queries/GetCourseList/GetCourseListQueryHandler.cs
+++ Src/csharp-mono/Application.Courses/Queries/GetCourseList/GetCourseListQueryHandler.cs
@@ -18,34 +18,33 @@ namespace Application.Courses.Queries.GetCourseList {
         public async Task<ApiResponse> Handle (GetCourseListQuery request, CancellationToken cancellationToken) {
             ApiResponse res = new ApiResponse ();
             try {
-                var courseQuery = await  _dbContext.Course.AsNoTracking().Where (x => x.IsDeleted == false).ToListAsync();
+                var courseQuery = await _dbContext.Course.AsNoTracking ().Where (x => x.IsDeleted == false).ToListAsync ();
                 if (!string.IsNullOrEmpty (request.search)) {
                     var courseSearchQuery = courseQuery.Where (x => x.IsDeleted == false &&
-                            (x.Code.ToLower ().Contains (request.search.ToLower ()) ||
-                                x.Name.ToLower ().Contains (request.search.ToLower ()) ||
-                                x.Id.ToString ().ToLower ().Contains (request.search.ToLower ()))
-                        ).Select (x => new {
-                            x.Id,
-                                x.Code,
-                                x.Name,
-                                x.Culture,
-                                x.Status,
-                        }).AsQueryable();
-                    res.totalcount = courseSearchQuery.Count();
+                        (x.Code.ToLower ().Contains (request.search.ToLower ()) ||
+                            x.Name.ToLower ().Contains (request.search.ToLower ()) ||
+                            x.Id.ToString ().ToLower ().Contains (request.search.ToLower ()))
+                    ).Select (x => new {
+                        x.Id,
+                            x.Code,
+                            x.Name,
+                            x.Culture,
+                            x.Status,
+                    }).AsQueryable ();
+                    res.totalcount = courseSearchQuery.Count ();
                     res.data = await courseSearchQuery.Skip (request.perPageRecord * request.pageNumber)
                         .Take (request.perPageRecord)
                         .OrderByDescending (x => x.Code)
                         .ToListAsync ();
                 } else {
-                    var courseSearchQuery = courseQuery.Where(x => x.IsDeleted == false).Select(x => new
-                    {
+                    var courseSearchQuery = courseQuery.Where (x => x.IsDeleted == false).Select (x => new {
                         x.Id,
-                        x.Code,
-                        x.Name,
-                        x.Culture,
-                        x.Status
-                    }).AsQueryable();
-                    res.totalcount = courseSearchQuery.Count();
+                            x.Code,
+                            x.Name,
+                            x.Culture,
+                            x.Status
+                    }).AsQueryable ();
+                    res.totalcount = courseSearchQuery.Count ();
                     res.data = await courseSearchQuery.Skip (request.perPageRecord * request.pageNumber)
                         .Take (request.perPageRecord)
                         .OrderByDescending (x => x.Code)
