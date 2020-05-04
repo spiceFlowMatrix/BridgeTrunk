6a8c280e3b657619a1fb21eb28f5ce55176bbdd6
poonamk <poonamk@SDD-29>
2020-05-04 12:06:18 +0530

    changes to create lesson controller

---
+++ Src/csharp-mono/Rest.Courses/Controllers/Lessons/LessonController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rest.Courses.Controllers.Lessons
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class LessonController : BaseController
+    {
+
+    }
+}
